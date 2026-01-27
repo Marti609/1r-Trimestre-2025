40a4deb4a612b69d8970f6c7ca0ccf054ac99a53
Marti609 <marti.galman@campus.xaloc.org>
2026-01-27 10:39:15 +0100

    python

---
+++ LISTAS/Ejercicio DNI.py
@@ -0,0 +1,8 @@
+#Diseñar un programa que al introducir un número calcule la letra correspondiente, teniendo en cuenta los siguientes puntos:
+dni=input("Introduce tu DNI: ")
+longitud_dni=len(dni)
+intentos=[]
+
+for i in range (len(dni))==8:
+
+
