7edbcc849f3d37cad67ab5f8a6f891922124aabc
Marti609 <marti.galman@campus.xaloc.org>
2026-02-20 15:21:20 +0100

    python

---
+++ JUTGE/JUTGE/P92351.py
@@ -0,0 +1,9 @@
+a, b=map(int, input().split()) 
+if b == 0:
+    print("No es pot dividir per 0")
+elif b>0:
+    cocient, resto = divmod(a, b)
+    print(f"{cocient} {resto}")
+elif b<0:
+    cocient, resto = divmod(a, b)
+    print(f"{cocient} {resto}")
